9bffde9299bd645dd8f125802add48818376d9d8
Varun Rao <varunrao1995@gmail.com>
2024-04-10 14:17:03 -0700

    Add sql to generate cc grouped by m0nth

---
+++ sql/cc_debt_month.sql
@@ -0,0 +1,14 @@
+# update account name
+
+SELECT
+  strftime ("%Y-%m", DATE) AS 'year-month',
+  TransactionType,
+  SUM(amount)
+FROM
+  transactions
+WHERE
+  AccountName = <accountname>  #  "Customized Cash Rewards Visa Signature - 4474", "Travel Rewards Visa Signature - 8166",...
+GROUP BY
+  1, 2
+ORDER BY 
+  1 ASC, 2 ASC;
